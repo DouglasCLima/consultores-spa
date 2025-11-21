4965f5cb822fc833a0e28fdcd53cddfd7bf4532f
DouglasCLima <143197856+DouglasCLima@users.noreply.github.com>
2025-11-21 10:32:19 -0300

    Create environment.prod.ts

---
+++ frontend-app/src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  // COLOQUE A URL DO RENDER AQUI!
+  apiUrl: '/api', 
+  firebase: {
+  apiKey: "AIzaSyBcXKNHgBG0z27kR4hEZ-Z0EzmHMXmtYsk",
+  authDomain: "meu-projeto-consultores.firebaseapp.com",
+  projectId: "meu-projeto-consultores",
+  storageBucket: "meu-projeto-consultores.firebasestorage.app",
+  messagingSenderId: "752812516972",
+  appId: "1:752812516972:web:44b076e317db4abe939a49",
+  measurementId: "G-ECC51PSE85"
+  }
+};
